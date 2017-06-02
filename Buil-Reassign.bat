75549b2dfa4561490b5a5a9904a8686cf1e8e54e
GitManeesh <chandra.maneesh@gmail.com>
2017-06-02 20:08:33 +0530

    first commit

--- Buil-Reassign.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo.
-call ant "E:\BankerChat\Workspace\BankerChatWithATUReporter-new\build.xml" setClassPath Generate-Report
-echo.
-
-
-start "" "E:\BankerChat\Workspace\BankerChatWithATUReporter-new\ATU Reporter\index.html"
-pause
